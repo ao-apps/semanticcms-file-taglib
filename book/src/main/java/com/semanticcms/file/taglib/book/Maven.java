bc842856b11a0bf5ccd9218e09b0368461597a9b
AO Industries, Inc. <support@aoindustries.com>
2017-07-15 16:10:16 -0500

    Using javadocs links from properties defined in parent pom.

--- book/src/main/java/com/semanticcms/file/taglib/book/Maven.java
+++ book/src/main/java/com/semanticcms/file/taglib/book/Maven.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-file-taglib - Files nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2016, 2017  AO Industries, Inc.
+ * Copyright (C) 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,32 +22,23 @@
  */
 package com.semanticcms.file.taglib.book;
 
-import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import com.aoindustries.util.PropertiesUtils;
+import java.io.IOException;
+import java.util.Properties;
 
 /**
  * @author  AO Industries, Inc.
  */
-public class SemanticCmsFileTldInitializer extends TagReferenceInitializer {
+class Maven {
 
-	private static final Map<String,String> additionalApiLinks = new LinkedHashMap<String,String>();
+	static final Properties properties;
 	static {
-		// Self
-		additionalApiLinks.put("com.semanticcms.file.taglib.", Maven.properties.getProperty("documented.url") + "apidocs/");
-		// Dependencies
-		additionalApiLinks.put("com.semanticcms.core.model.", "https://semanticcms.com/core/model/apidocs/");
+		try {
+			properties = PropertiesUtils.loadFromResource(Maven.class, "Maven.properties");
+		} catch(IOException e) {
+			throw new ExceptionInInitializerError(e);
+		}
 	}
 
-	public SemanticCmsFileTldInitializer() {
-		super(
-			"File Taglib Reference",
-			"Taglib Reference",
-			"/file/taglib",
-			"/semanticcms-file.tld",
-			Maven.properties.getProperty("javac.link.javaApi.jdk16"),
-			Maven.properties.getProperty("javac.link.javaeeApi.6"),
-			additionalApiLinks
-		);
-	}
+	private Maven() {}
 }
