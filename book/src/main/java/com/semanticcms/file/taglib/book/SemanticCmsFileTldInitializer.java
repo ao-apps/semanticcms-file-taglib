1187ca8aae9c3866c777264dd26ada9a08799738
AO Industries, Inc <support@aoindustries.com>
2020-06-03 17:13:41 -0500

    Now using String varargs instead of generic Map

--- book/src/main/java/com/semanticcms/file/taglib/book/SemanticCmsFileTldInitializer.java
+++ book/src/main/java/com/semanticcms/file/taglib/book/SemanticCmsFileTldInitializer.java
@@ -23,11 +23,9 @@
 package com.semanticcms.file.taglib.book;
 
 import com.semanticcms.tagreference.TagReferenceInitializer;
-import java.util.Collections;
 
 public class SemanticCmsFileTldInitializer extends TagReferenceInitializer {
 
-	@SuppressWarnings("unchecked")
 	public SemanticCmsFileTldInitializer() {
 		super(
 			Maven.properties.getProperty("project.name") + " Reference",
@@ -38,9 +36,9 @@ public class SemanticCmsFileTldInitializer extends TagReferenceInitializer {
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
-			Collections.singletonMap("com.semanticcms.file.taglib", Maven.properties.getProperty("project.url") + "apidocs/"),
+			"com.semanticcms.file.taglib", Maven.properties.getProperty("project.url") + "apidocs/",
 			// Dependencies
-			Collections.singletonMap("com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/")
+			"com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/"
 		);
 	}
 }
