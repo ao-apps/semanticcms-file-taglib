bb4de5463a16dd14c162e5650485c7c0180940fa
AO Industries, Inc <support@aoindustries.com>
2021-02-19 09:08:39 -0600

    Renamed Html to Document

--- src/main/java/com/semanticcms/file/taglib/FileTag.java
+++ src/main/java/com/semanticcms/file/taglib/FileTag.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-file-taglib - Files nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020  AO Industries, Inc.
+ * Copyright (C) 2013, 2014, 2015, 2016, 2017, 2019, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -23,7 +23,7 @@
 package com.semanticcms.file.taglib;
 
 import com.aoindustries.encoding.taglib.EncodingBufferedTag;
-import com.aoindustries.html.servlet.HtmlEE;
+import com.aoindustries.html.servlet.DocumentEE;
 import com.aoindustries.io.buffer.BufferResult;
 import com.aoindustries.io.buffer.BufferWriter;
 import static com.aoindustries.taglib.AttributeUtils.resolveValue;
@@ -98,7 +98,7 @@ public class FileTag extends ElementTag<File> {
 					servletContext,
 					request,
 					response,
-					(capturedOut == null) ? null : HtmlEE.get(servletContext, request, response, capturedOut),
+					(capturedOut == null) ? null : DocumentEE.get(servletContext, request, response, capturedOut),
 					file
 				);
 			} finally {
