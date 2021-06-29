c699e561318d2052c5d6693094db4adb73a84c51
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:45:09 -0500

    Now supports Java 9+ modules.

--- book/src/main/java/com/semanticcms/file/taglib/book/SemanticCmsFileTldInitializer.java
+++ book/src/main/java/com/semanticcms/file/taglib/book/SemanticCmsFileTldInitializer.java
@@ -46,9 +46,9 @@ public class SemanticCmsFileTldInitializer extends TagReferenceInitializer {
 			Maven.properties.getProperty("documented.javadoc.link.javase"),
 			Maven.properties.getProperty("documented.javadoc.link.javaee"),
 			// Self
-			"com.semanticcms.file.taglib", Maven.properties.getProperty("project.url") + "apidocs/",
+			"com.semanticcms.file.taglib", Maven.properties.getProperty("project.url") + "apidocs/com.semanticcms.file.taglib/",
 			// Dependencies
-			"com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/"
+			"com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/com.semanticcms.core.model/"
 		);
 	}
 }
