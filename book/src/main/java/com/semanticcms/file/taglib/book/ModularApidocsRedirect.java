cfa4cf84102206d8ddc57dc96f0b6969eda03da1
AO Industries, Inc <support@aoindustries.com>
2021-06-29 02:41:50 -0500

    Now supports Java 9+ modules.

--- book/src/main/java/com/semanticcms/file/taglib/book/ModularApidocsRedirect.java
+++ book/src/main/java/com/semanticcms/file/taglib/book/ModularApidocsRedirect.java
@@ -1,6 +1,6 @@
 /*
  * semanticcms-file-taglib - Files nested within SemanticCMS pages and elements in a JSP environment.
- * Copyright (C) 2016, 2017, 2019, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -22,23 +22,27 @@
  */
 package com.semanticcms.file.taglib.book;
 
-import com.semanticcms.tagreference.TagReferenceInitializer;
+import com.aoapps.net.URIParametersUtils;
+import com.aoapps.servlet.http.HttpServletUtil;
+import java.io.IOException;
+import java.util.Objects;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class SemanticCmsFileTldInitializer extends TagReferenceInitializer {
+@WebServlet("/file/taglib/apidocs/com/*")
+public class ModularApidocsRedirect extends HttpServlet {
 
-	public SemanticCmsFileTldInitializer() {
-		super(
-			Maven.properties.getProperty("documented.name") + " Reference",
-			"Taglib Reference",
-			"/file/taglib",
-			"/semanticcms-file.tld",
-			true,
-			Maven.properties.getProperty("documented.javadoc.link.javase"),
-			Maven.properties.getProperty("documented.javadoc.link.javaee"),
-			// Self
-			"com.semanticcms.file.taglib", Maven.properties.getProperty("project.url") + "apidocs/com.semanticcms.file.taglib/",
-			// Dependencies
-			"com.semanticcms.core.model", "https://semanticcms.com/core/model/apidocs/com.semanticcms.core.model/"
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpServletUtil.sendRedirect(
+			HttpServletResponse.SC_MOVED_PERMANENTLY, req, resp,
+			"/file/taglib/apidocs/com.semanticcms.file.taglib/com" + Objects.toString(req.getPathInfo(), ""),
+			URIParametersUtils.of(req.getQueryString()), true, false
 		);
 	}
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,40 @@
+/*
+ * semanticcms-file-taglib - Files nested within SemanticCMS pages and elements in a JSP environment.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of semanticcms-file-taglib.
+ *
+ * semanticcms-file-taglib is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * semanticcms-file-taglib is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with semanticcms-file-taglib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.semanticcms.file.taglib {
+	exports com.semanticcms.file.taglib;
+	// Direct
+	requires com.aoapps.encoding.taglib; // <groupId>com.aoapps</groupId><artifactId>ao-encoding-taglib</artifactId>
+	requires com.aoapps.html.any; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-any</artifactId>
+	requires com.aoapps.html.servlet; // <groupId>com.aoapps</groupId><artifactId>ao-fluent-html-servlet</artifactId>
+	requires com.aoapps.io.buffer; // <groupId>com.aoapps</groupId><artifactId>ao-io-buffer</artifactId>
+	requires com.aoapps.servlet.filter; // <groupId>com.aoapps</groupId><artifactId>ao-servlet-filter</artifactId>
+	requires com.aoapps.taglib; // <groupId>com.aoapps</groupId><artifactId>ao-taglib</artifactId>
+	requires javax.el.api; // <groupId>javax.el</groupId><artifactId>javax.el-api</artifactId>
+	requires javax.servlet.api; // <groupId>javax.servlet</groupId><artifactId>javax.servlet-api</artifactId>
+	requires javax.servlet.jsp.api; // <groupId>javax.servlet.jsp</groupId><artifactId>javax.servlet.jsp-api</artifactId>
+	requires com.semanticcms.core.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-model</artifactId>
+	requires com.semanticcms.core.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-servlet</artifactId>
+	requires com.semanticcms.core.taglib; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-core-taglib</artifactId>
+	requires com.semanticcms.file.model; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-model</artifactId>
+	requires com.semanticcms.file.servlet; // <groupId>com.semanticcms</groupId><artifactId>semanticcms-file-servlet</artifactId>
+}
