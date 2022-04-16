984156baebb86907a65cc17b73684bee7ea9deef
AO Industries, Inc <support@aoindustries.com>
2022-04-16 02:02:41 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/semanticcms/file/taglib/Functions.java
+++ src/main/java/com/semanticcms/file/taglib/Functions.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with semanticcms-file-taglib.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.semanticcms.file.taglib;
 
 import static com.aoapps.servlet.filter.FunctionContext.getRequest;
